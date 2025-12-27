1af81583210011affd7a48f2a1561061584f362f
mehmetefecelebi <mehmetefecelebi@posta.mu.edu.tr>
2025-12-27 18:15:25 +0300

    Python Tasks dosyası eklendi

---
+++ Python Tasks/Week5/Dot1003_240517016_MehmetEfeÇelebi_Task54.py
@@ -0,0 +1,8 @@
+name ="The quick brown fox jumps over the lazy dog"
+harf = input("Which item do you want to search? ")
+print(name.count(harf))
+print(name[-3:])
+
+
+
+   
---
+++ Python Tasks/Week5/Dot1003_240517016_MehmetEfeÇelebi_Task60.py
@@ -0,0 +1,5 @@
+my_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(my_matrix[2][1])
+for row in my_matrix:
+    print(my_matrix)
+    print(my_matrix[2][0])
